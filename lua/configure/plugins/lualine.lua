ca7da51958929ba5825b0e034cd07ac80a7a1bff
askfiy <c2323182108@gmail.com>
2022-04-10 01:17:40 +0800

    add lualine priority

--- lua/configure/plugins/lualine.lua
+++ lua/configure/plugins/lualine.lua
@@ -1,10 +1,23 @@
 -- https://github.com/nvim-lualine/lualine.nvim
 -- https://github.com/SmiteshP/nvim-gps
+
 local icons = require("utils.icons")
 
 local gps = require("nvim-gps")
 
 gps.setup()
+
+local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
+    return function(str)
+        local win_width = vim.fn.winwidth(0)
+        if hide_width and win_width < hide_width then
+            return ""
+        elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+            return str:sub(1, trunc_len) .. (no_ellipsis and "" or "...")
+        end
+        return str
+    end
+end
 
 local function hide_component()
     local filetype_table = {
@@ -44,14 +57,31 @@ require("lualine").setup(
                         dos = " ", -- e70f
                         mac = " " -- e711
                     },
-                    cond = hide_component
+                    cond = hide_component,
+                    fmt = trunc(12, 0, nil, true)
                 },
-                {"mode", cond = hide_component},
-                {"filetype", cond = show_component}
+                {
+                    "mode",
+                    cond = hide_component,
+                    fmt = trunc(22, 0, nil, true)
+                },
+                {
+                    "filetype",
+                    cond = show_component,
+                    fmt = trunc(12, 0, nil, true)
+                }
             },
             lualine_b = {
-                {"branch", cond = hide_component},
-                {"diff", cond = hide_component},
+                {
+                    "branch",
+                    cond = hide_component,
+                    fmt = trunc(70, 0, nil, true)
+                },
+                {
+                    "diff",
+                    cond = hide_component,
+                    fmt = trunc(85, 0, nil, true)
+                },
                 {
                     "diagnostics",
                     symbols = {
@@ -60,29 +90,64 @@ require("lualine").setup(
                         info = icons.diagnostics.info,
                         hint = icons.diagnostics.hint
                     },
-                    cond = hide_component
+                    cond = hide_component,
+                    fmt = trunc(90, 0, nil, true)
                 }
             },
             lualine_c = {
-                {"filename", cond = hide_component},
-                {gps.get_location, cond = gps.is_available}
+                {
+                    "filename",
+                    cond = hide_component,
+                    fmt = trunc(37, 0, nil, true)
+                },
+                {
+                    gps.get_location,
+                    cond = gps.is_available,
+                    fmt = trunc(120, 0, nil, true)
+                }
             },
             lualine_x = {
-                {"filetype", cond = hide_component}
+                {
+                    "filetype",
+                    cond = hide_component,
+                    fmt = trunc(42, 0, nil, true)
+                }
             },
             lualine_y = {
-                {"encoding", cond = hide_component}
+                {
+                    "encoding",
+                    cond = hide_component,
+                    fmt = trunc(52, 0, nil, true)
+                }
             },
             lualine_z = {
-                {"location", cond = hide_component},
-                {"progress", cond = hide_component}
+                {
+                    "location",
+                    cond = hide_component,
+                    fmt = trunc(61, 0, nil, true)
+                },
+                {
+                    "progress",
+                    cond = hide_component,
+                    fmt = trunc(12, 0, nil, true)
+                }
             }
         },
         inactive_sections = {
             lualine_a = {},
             lualine_b = {},
-            lualine_c = {"filename"},
-            lualine_x = {"location"},
+            lualine_c = {
+                {
+                    "filename",
+                    fmt = trunc(10, 0, nil, true)
+                }
+            },
+            lualine_x = {
+                {
+                    "location",
+                    fmt = trunc(20, 0, nil, true)
+                }
+            },
             lualine_y = {},
             lualine_z = {}
         },
