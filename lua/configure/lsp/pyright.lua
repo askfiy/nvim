0ed6b6b348344b1f06714e7beacc968978c2f240
askfiy <c2323182108@gmail.com>
2022-05-24 17:29:58 +0800

    Update pyright diagnostic level

--- lua/configure/lsp/pyright.lua
+++ lua/configure/lsp/pyright.lua
@@ -53,19 +53,27 @@ M.lsp_config = {
     settings = {
         python = {
             analysis = {
-                typeCheckingMode = "basic", -- off, basic, strict
+                typeCheckingMode = "off", -- off, basic, strict
                 useLibraryCodeForTypes = true,
                 autoImportCompletions = true,
                 diagnosticMode = "workspace",
+                -- https://github.com/microsoft/pyright/blob/main/docs/configuration.md#type-check-diagnostics-settings
                 diagnosticSeverityOverrides = {
                     strictListInference = true,
                     strictDictionaryInference = true,
                     strictSetInference = true,
-                    strictParameterNoneValue = true,
+                    reportUnusedImport = "warning",
+                    reportUnusedClass = "warning",
+                    reportUnusedFunction = "warning",
+                    reportUnusedVariable = "warning",
+                    reportUnusedCoroutine = "warning",
+                    reportDuplicateImport = "warning",
+                    reportPrivateUsage = "warning",
+                    reportUnusedExpression = "warning",
+                    reportConstantRedefinition = "error",
+                    reportIncompatibleMethodOverride = "error",
                     reportUndefinedVariable = "error",
-                    reportMissingTypeStubs = "none",
-                    reportIncompleteStub = "none",
-                    reportInvalidStubStatement = "none",
+                    reportAssertAlwaysTrue = "error",
                 },
             },
         },
