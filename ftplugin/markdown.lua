7e2990845a2351eea7c4d534ef3a5caea5752826
askfiy <c2323182108@gmail.com>
2022-07-01 23:33:11 +0800

    Add temporary fix for markdown misspelling

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -2,5 +2,7 @@ vim.bo.expandtab = true
 vim.bo.shiftwidth = 4
 vim.bo.tabstop = 4
 vim.bo.softtabstop = 4
+-- FIX: https://github.com/lewis6991/spellsitter.nvim/issues/75
+vim.opt_local.spell = false
 vim.opt_local.foldmethod = "expr"
 vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
